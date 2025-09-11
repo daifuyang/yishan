6b5e7a54e272d0ebaeff8884a89e2ee1bc549293
daifuyang <1140444693@qq.com>
2025-09-11 23:32:52 +0800

    feat(date-picker): 优化日期选择器样式和功能
    - 添加 PickerPanel 组件支持
    - 使用自定义 Input 组件替换默认输入框
    - 更新样式以匹配设计规范
    - 调整开发脚本使用 chokidar 监听文件变化
    - 添加相关依赖支持新功能

--- packages/shadcn/components/ui/date-picker.tsx
+++ packages/shadcn/components/ui/date-picker.tsx
@@ -1,15 +1,29 @@
-import Picker from "rc-picker";
+import Picker, { PickerPanel } from "rc-picker";
+import 'dayjs/locale/zh-cn';
 import dayjsGenerateConfig from "rc-picker/es/generate/dayjs";
 import zhCN from "rc-picker/es/locale/zh_CN";
+import { Input } from "./input";
+
+console.log("zhCN", zhCN);
 
 function DatePicker() {
   return (
-    <Picker
-      prefixCls="shadcn-date-picker"
-      generateConfig={dayjsGenerateConfig}
-      locale={zhCN}
-      allowClear
-    />
+    <>
+      <Picker
+        prefixCls="shadcn-date-picker"
+        generateConfig={dayjsGenerateConfig}
+        locale={zhCN}
+        components={{
+          input: (props) => <Input {...props} />,
+        }}
+        allowClear
+      />
+      <PickerPanel
+        prefixCls="shadcn-date-picker"
+        generateConfig={dayjsGenerateConfig}
+        locale={zhCN}
+      />
+    </>
   );
 }
 
